71c2cfe665e901ace9655973034ba1cee3914e10
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-09-14 17:56:13 +0200

    feat(common-ui): styles for button

--- libs/common-ui/src/lib/atoms/Button/Button.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps, FormEventHandler } from 'react';
+import clsx from 'clsx';
 // colors from palette: https://flatuicolors.com/palette/defo
 // to demonstrate there can be also color a set
 // 💡 we can also create colors using Record utility type but need to specify all keys
@@ -71,10 +72,20 @@ export const Button = ({
   const _color = color ? colors[color] : '';
   const _bgColor = bgColor ? colors[bgColor] : '';
 
+  const classes = clsx(
+    'px-4 py-1',
+    'text-sm text-white',
+    'bg-blue-600',
+    'font-semibold',
+    'rounded-full border border-blue-200',
+    'hover:text-white hover:bg-blue-500 hover:border-transparent',
+    className
+  );
+
   return (
     <button
       onClick={onClick}
-      className={className}
+      className={classes}
       style={{
         color: _color,
         backgroundColor: _bgColor,
