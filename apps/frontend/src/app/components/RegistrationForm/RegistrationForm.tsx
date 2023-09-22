db8eb7ebbc0ebd83ada83b0cdb4a69f172b8f2b3
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-09-22 16:31:24 +0200

    feat: refactor state to ref

--- apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
+++ apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
@@ -1,41 +1,35 @@
-import { FormEventHandler, useState } from 'react';
+import { FormEventHandler, useRef } from 'react';
 import { Button } from '@ems/common-ui';
 
 import './RegistrationForm.module.css';
 
 export const RegistrationForm = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [age, setAge] = useState(0);
+  // const [firstName, setFirstName] = useState('');
+  // const [lastName, setLastName] = useState('');
+  // const [age, setAge] = useState(0);
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
-    console.log({ firstName, lastName, age });
+    // console.log({ firstName, lastName, age });
+    console.log({ firsName: firstNameRef.current?.value });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="firstName">First name: </label>
-        <input
-          id="firstName"
-          onChange={(event) => setFirstName(event.target.value)}
-        />
+        <input id="firstName" ref={firstNameRef} />
       </div>
       <div>
         <label htmlFor="lastName">Last name: </label>
-        <input
-          id="lastName"
-          onChange={(event) => setLastName(event.target.value)}
-        />
+        <input id="lastName" ref={lastNameRef} />
       </div>
       <div>
         <label htmlFor="age">Age: </label>
-        <input
-          id="age"
-          type="number"
-          onChange={(event) => setAge(parseInt(event.target.value, 10))}
-        />
+        <input id="age" type="number" ref={ageRef} />
       </div>
       <Button type="submit" label="Send" />
     </form>
