4ccce8480419e1f55ac0ff486e3d0be7199cf502
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-02 15:13:40 +0200

    feat: watched value

--- apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
+++ apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
@@ -1,34 +1,44 @@
-import { FormEventHandler, useRef } from 'react';
+import { FormEventHandler, useEffect, useRef, useState } from 'react';
 import { Button, Input } from '@ems/common-ui';
 
 import './RegistrationForm.module.css';
 
 export const RegistrationForm = () => {
-  // const [firstName, setFirstName] = useState('');
-  // const [lastName, setLastName] = useState('');
-  // const [age, setAge] = useState(0);
+  const [age, setAge] = useState(0);
   const firstNameRef = useRef<HTMLInputElement>(null);
   const lastNameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  let adult = false;
 
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
-    // console.log({ firstName, lastName, age });
     console.log({ firstName: firstNameRef.current?.value });
   };
 
+  useEffect(() => {
+    adult = age > 18;
+  }, [age]);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
       <div>
-        <Input label="First name: " ref={firstNameRef} />
+        Current age: {age} ({adult ? 'YES' : 'NO'})
       </div>
-      <div>
-        <Input label="Last name: " ref={lastNameRef} />
-      </div>
-      <div>
-        <Input type="number" label="Age: " ref={ageRef} />
-      </div>
-      <Button type="submit" label="Send" />
-    </form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Input label="First name: " ref={firstNameRef} />
+        </div>
+        <div>
+          <Input label="Last name: " ref={lastNameRef} />
+        </div>
+        <div>
+          <Input
+            type="number"
+            label="Age: "
+            onChange={(e) => setAge(parseInt(e.target.value, 10))}
+          />
+        </div>
+        <Button type="submit" label="Send" />
+      </form>
+    </div>
   );
 };
