fb0c5c494c471d839702991285cf0a76a290e169
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-02-18 15:51:23 +0100

    feat: menu with server and client componetns

--- apps/website/components/Menu/Menu.tsx
+++ apps/website/components/Menu/Menu.tsx
@@ -1,33 +1,45 @@
+'use client';
+
+import { classMerge } from '@ems/common-ui';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const Menu = () => {
+  const pathName = usePathname();
+
+  // console.log({ pathName });
+
+  const buildCssClass = (path: string) => {
+    return classMerge('text-lg', { 'text-red-400': path === pathName });
+  };
+
   return (
     <nav className="bg-slate-900 text-white">
       <div className="container mx-auto p-4">
         <div className="flex justify-between">
           <ul className="flex">
             <li className="mr-6">
-              <Link href="/about" className="text-lg">
+              <Link href="/about" className={buildCssClass('/about')}>
                 About
               </Link>
             </li>
             <li className="mr-6">
-              <Link href="/job-offers" className="text-lg">
+              <Link href="/job-offers" className={buildCssClass('/job-offers')}>
                 Job offers
               </Link>
             </li>
             <li className="mr-6">
-              <Link href="/reviews" className="text-lg">
+              <Link href="/reviews" className={buildCssClass('/reviews')}>
                 Reviews
               </Link>
             </li>
             <li className="mr-6">
-              <Link href="/contact" className="text-lg">
+              <Link href="/contact" className={buildCssClass('/contact')}>
                 Contact
               </Link>
             </li>
             <li className="mr-6">
-              <Link href="/dashboard" className="text-lg">
+              <Link href="/dashboard" className={buildCssClass('/dashboard')}>
                 Dashboard
               </Link>
             </li>
