4292f74e584113e11bcb742ca36ef7ceb18b49c7
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-03-03 21:03:46 +0100

    feat: fetching on server side

--- apps/website/app/(public)/reviews/page.tsx
+++ apps/website/app/(public)/reviews/page.tsx
@@ -1,12 +1,23 @@
 import { Header } from '@ems/common-ui';
 import { ReviewsList } from './ReviewsList';
+import { fetchReviews } from './services';
 
-export default function AboutPage() {
+export default async function ReviewsPage() {
+  const reviews = await fetchReviews();
   return (
     <div>
       <Header>Reviews</Header>
       <p>All reviews</p>
-      <ReviewsList />
+      {/* <ReviewsList /> */}
+      <ul>
+        {reviews?.map((elem) => (
+          <li key={elem.id}>
+            <div>{elem.content}</div>
+            <div className="font-light">{elem.author}</div>
+            <div className="mb-4 font-light">{elem.created_at}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
